a81777b10eb3d8dc23f0eba0bc3296cc77da0d3f
Carlos Tello <ctellob3114@gmail.com>
2026-07-01 08:58:26 -0500

    Commit Clase Maps y practicaEva

--- src/Evaluacion/PersonaController.java
+++ src/Evaluacion/PersonaController.java
@@ -3,10 +3,20 @@ package Evaluacion;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class PersonaController {
     // Filtrar de todas las personas que llegan, las que son superiores al umbral
     public Set<Persona> filtrarYOrdenar(List<Persona> personas, int edadUmbral) {
+        Set<Persona> personasFiltradas = new TreeSet<>(
+                (p1, p2) -> {
+                    int compE = Integer.compare(p1.getEdad(), p2.getEdad());
+                    if (compE != 0) {
+                        return compE;
+                    }
+                    int compN = p1.getNombre().compareToIgnoreCase(p2.getNombre());
+                    return compN;
+                });
         return null;
     }
 
